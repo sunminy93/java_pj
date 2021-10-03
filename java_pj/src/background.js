2531e52c9291c58c6eda0b4ac167a26dff2a0057
sunminy93 <88812034+sunminy93@users.noreply.github.com>
2021-10-03 22:44:46 +0900

    Delete java_pj directory

--- java_pj/src/background.js
+++
@@ -1,9 +0,0 @@
-const imgs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
-
-
-const randomImg = imgs[Math.floor(Math.random() * imgs.length)];
-const cssBack = document.createElement("img");
-cssBack.src = `img/${randomImg}`;
-document.body.appendChild(cssBack);
-
-
